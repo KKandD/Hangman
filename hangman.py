0278363760e1d0eab99cc8933473af06bd028752
Izabela Żmuda <izabela.oleksiak@gmail.com>
2020-10-11 17:38:53 +0200

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -5,8 +5,17 @@ list_of_countries = open("countries.txt").read().splitlines()
 #losowanie 1 kraju z listy
 selected_country = random.choice(list_of_countries)
 #print(selected_country)
-print(len(selected_country))
+#zostawiam do sprawdzania, potem usuniemy
 
+#zliczenie liczby liter w wylosowanym wyrazie (chyba nie bedzie to będzie potrzebne, na razie zostawiam, najwyzej usuniemy)
+number_of_letters = len(selected_country)
+#print(number_of_letters)
+# zostawiam do sprawdzania, potem usuniemy
+
+#zamiana string ze słowem na listę
+list_of_letters = list(selected_country)
+#print(list_of_letters)
+# zostawiam do sprawdzania, potem usuniemy
 
 def lives_count():
     if level_dificultity == 1:
