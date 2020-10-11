0f6122c9149ce18779f5e3dee9009a89b0f2b8c7
Izabela Żmuda <izabela.oleksiak@gmail.com>
2020-10-11 17:26:13 +0200

    Update hangman.py
    losowanie kraju z listy, zliczanie liczby znaków w wylosowanym wyrazie

--- hangman.py
+++ hangman.py
@@ -1,6 +1,12 @@
+import random
+#wczytuję listę krajów z pliku
 list_of_countries = open("countries.txt").read().splitlines()
 
-print(list_of_countries)
+#losowanie 1 kraju z listy
+selected_country = random.choice(list_of_countries)
+#print(selected_country)
+print(len(selected_country))
+
 
 def lives_count():
     if level_dificultity == 1:
