0732a9e7536b521cb21aadd1f605c29433bb87eb
KKandD <63910995+KKandD@users.noreply.github.com>
2020-10-11 16:18:39 +0200

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,12 @@
+
+
+def lives_count():
+    if level_dificultity == 1:
+        lives = 6
+    elif level_dificultity == 2:
+        lives = 4
+    elif level_dificultity == 3:
+        lives = 2
+
+def level_dificultity():
+    pass
